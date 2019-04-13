17e7cb1eb5aff0bbde619d57611b8e1737c95a36
Rahuvich <raulmateob@gmail.com>
2019-04-13 20:58:51 +0200

    Now the web can make sums

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,16 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  querySelector('#output').text = 'Your result will be displayed here';
+
+
+  querySelector('#num1').onChange.listen(func);
+  querySelector('#num2').onChange.listen(func);
+}
+
+void func(Event event){
+  int num1 = int.parse((querySelector('#num1') as InputElement).value);
+  int num2 = int.parse((querySelector('#num2') as InputElement).value);
+
+  querySelector('#output').text = (num1 + num2).toString();
 }
