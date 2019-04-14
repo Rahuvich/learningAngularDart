aaf735cbe70c47d13193ccca94fbeb44526f4b70
Rahuvich <raulmateob@gmail.com>
2019-04-14 17:16:11 +0200

    Now this app converts a decimal number to binary, hex, and oct

--- web/main.dart
+++ web/main.dart
@@ -2,12 +2,14 @@ import 'dart:html';
 
 void main() {
   Element title = querySelector('#title');
-  title.text = "This app converts your decimal number to binary";
+  title.text = "DEC to BIN, HEX, OCT converter";
   title.style.textAlign = "center";
   title.style.fontSize = "28px";
   title.style.padding = "20px";
 
-  querySelector('#output').text = 'Your result will be displayed here';
+  querySelector('#bin').text = 'Your binary result will be displayed here';
+  querySelector('#hex').text = 'Your hexadecimal result will be displayed here';
+  querySelector('#oct').text = 'Your octadecimal will be displayed here';
 
 
   querySelector('#num1').onChange.listen(func);
@@ -15,13 +17,16 @@ void main() {
 
 void func(Event event){
   int num1 = int.parse((querySelector('#num1') as InputElement).value);
+  int aux = num1;
 
   int bin = 0, i = 1;
-  while(num1 > 0){
-    bin = bin + (num1 % 2)*i;
-    num1 = (num1/2).floor();
+  while(aux > 0){
+    bin = bin + (aux % 2)*i;
+    aux = (aux/2).floor();
     i = i*10;
   }
 
-  querySelector('#output').text = (bin).toString();
+  querySelector('#bin').text = (bin).toString();
+  querySelector('#hex').text = num1.toRadixString(16).padLeft(4, '0').toUpperCase();
+  querySelector('#oct').text = num1.toRadixString(8).padLeft(4, '0').toUpperCase();
 }
